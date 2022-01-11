f8c70b7cefc465c880d9a518068fca75766984a2
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-01-11 18:14:57 -0300

    Update Motors

--- Demo/Learning/Motors.py
+++ Demo/Learning/Motors.py
@@ -7,6 +7,25 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-# Create your objects here.
+# Create your objects here. - Initialization Process
 
+ev3 = EV3Brick()
+medium_motor = Motor(Port.D)
+left_wheel = Motor(Port.B)
+right_wheel = Motor(Port.C)
+robot = DriveBase(left_wheel,right_wheel,wheel_diameter=55.5,axle_track=104)
 # Write your program here.
+medium_motor.run_target(2000,250)
+left_wheel.run_target(3000,390)
+robot.turn(110)
+robot.turn(-110)
+robot.straight(250)
+robot.straight(-250)
+robot.stop()
+left_wheel.break()
+right_wheel.break()
+for c in range (0,3):
+    robot.straight(360)
+    robot.stop()
+    left_wheel.break()
+    right_wheel.break()
