a36ac2dcc458f80ff60cd7dcc48ed36154913b6e
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-01-13 22:20:07 -0300

    Final

--- Demo/Learning/Flow_Control.py
+++ Demo/Learning/Flow_Control.py
@@ -18,10 +18,34 @@ robot = DriveBase(left, right, wheel_diameter=55.5, axle_track=104)
 
 #Loop - Forever
 
-while True
+while True:
 
 #Loop - Condition 
 
 while robot.distance() <300:
     robot.drive(250,0)
-    
+robot.stop
+left.brake()
+right.brake()
+
+#If e Else - Condition
+
+while True:
+    if Button.CENTER in ev3.buttons.pressed():
+        robot.straight(-200)
+        robot.straight(200)
+    elif Button.RIGHT in ev3.buttons.pressed():
+        robot.straight(-300)
+        robot.straight(300)
+    else:
+        robot.straight(200)
+        robot.straight(-200)
+while robot.distance() >= 500:
+    robot.drive(-200,0)
+robot.stop()
+right.brake()
+left.brake()
+
+#Wait 
+
+wait(2000)
